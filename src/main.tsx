ceccf031e39a6660e4ee276c4b38b0620ae6c672
essarebano <essarebano.dev@gmail.com>
2024-06-12 14:18:31 +0800

    Fixed ESLint

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,6 @@ const router = createBrowserRouter([{
   }]
 }])
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
